896f9dc91b97c0b9095c3b9455d850aee016fa5c
xc-kiwiberry <xc_kiwiberry@qq.com>
2020-10-28 21:12:45 +0800

    fix minor
    fix a potential bug: while the eos_id != 1 the translation will be unable to stop.

--- thumt/bin/translator.py
+++ thumt/bin/translator.py
@@ -132,9 +132,11 @@ def convert_to_string(tensor, params):
     ids = tensor.tolist()
 
     output = []
+    
+    eos_id = params.lookup["target"][params.eos.encode("utf-8")]
 
     for wid in ids:
-        if wid == 1:
+        if wid == eos_id:
             break
         output.append(params.mapping["target"][wid])
 
